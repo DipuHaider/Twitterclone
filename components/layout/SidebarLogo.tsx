ed1cfcbc34bcbd360d3a6dedfebdbbfb856c19c2
Dipu Haider <dipuhaider@gmail.com>
2023-04-29 16:52:27 +0600

    Update SidebarLogo.tsx

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -4,8 +4,8 @@ const SidebarLogo = () => {
     const router = useRouter();
 
     return (
-        <div>
-
+        <div className='rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition'>
+            
         </div>
     );
 }
